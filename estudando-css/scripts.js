0d543af97bdd625eac253aef2d59104848624b4e
Patrick <patrickamaralr1@gmail.com>
2022-12-24 11:35:17 -0300

    iniciando projeto

---
+++ estudando-css/scripts.js
@@ -0,0 +1,73 @@
+/* function pontuar(nota) {
+    if(nota >= 90) {
+        console.log('A')
+    }
+    
+    if(nota >= 80 && nota <= 89) {
+        console.log('B')
+    }
+    
+    if(nota >= 70 && nota <= 79) {
+        console.log('C')
+    }
+    
+    if(nota >= 60 && nota <= 69) {
+        console.log('D')
+    }
+    
+    if(nota <= 60) {
+        console.log('F')
+    }
+}
+const notas = [85, 20, 44, 60, 77, 90, 12, 10]
+
+notas.forEach(nota => pontuar(nota)) */
+
+
+
+/* let contas = {
+    receitas: [250, 100, 1000],
+    despesas: [120, 700, 1500]
+}
+
+function calcular(conta) {
+    let totalReceita = 0
+    conta.receitas.forEach(element => {
+        totalReceita = totalReceita + element
+    });
+    let totalDespesa = 0
+    conta.despesas.forEach(element => {
+        totalDespesa = totalDespesa + element
+    })
+
+    var saldo = totalReceita - totalDespesa
+
+    if (saldo > 0) {
+        console.log(`O saldo atual é ${saldo} Positivo`)
+    }
+    if (saldo <= 0) {
+        console.log(`O saldo atual é ${saldo} Negativo`)
+    }
+
+}
+
+calcular(contas) */
+
+
+
+/* let temperatura = 100
+
+
+function CelsiusToFahrenheit(celsius) {
+    return celsius * 9/5 + 32   
+}
+
+function FahrenheitToCelsius(fahrenheit) {
+    return fahrenheit - 32 * 5/9
+}
+
+let fahrenheit = CelsiusToFahrenheit(temperatura)
+
+let celsius = FahrenheitToCelsius(temperatura)
+
+console.log(celsius, fahrenheit) */
---
+++ typescript-project/project.ts
@@ -0,0 +1,42 @@
+  /* class Veiculos {
+    modelo: string
+    rodas: number
+    constructor(modelo: string, rodas: number) {
+        this.modelo = modelo
+        this.rodas = rodas
+    }
+}
+
+let toyota = new Veiculos("10", 10)
+
+console.log(toyota.modelo, toyota.rodas)
+
+toyota.modelo = "corola"
+
+console.log(toyota.modelo, toyota.rodas)
+
+class Motos extends Veiculos {
+    constructor(modelo: string, rodas: number) {
+        super(modelo, rodas)
+    }
+}
+
+let kawasaki = new Motos("5", 2)
+
+console.log(kawasaki instanceof Veiculos, kawasaki.modelo, kawasaki.rodas)
+
+type exemplo = "fruta" | "carne"
+let patrick: exemplo
+patrick = "fruta" */
+
+let ourTuple: [number, boolean, string];
+
+ourTuple = [5, false, 'Coding God was here'];
+
+ourTuple.push('Something new and wrong');
+
+console.log(ourTuple);
+
+let nome: any = "Henrique Correa do Amaral"
+nome = nome.split('')
+console.log(nome)
